5a5cd2f94dc8e745dc06d4982c596fc3a40f3a23
cak <cak@fastmail.com>
2024-03-29 06:37:01 -0400

    update navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
   useEffect(() => {
-    window.location.replace("/events");
-  });
+    router.replace("events");
+  }, [router]);
 
   return <></>;
 }
