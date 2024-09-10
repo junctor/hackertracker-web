b61cd0910f2c953cd7d1389489e0711e67173416
cak <cak@fastmail.com>
2024-09-10 05:41:48 -0400

    typescript fixes / formatting

--- src/components/people/people.tsx
+++ src/components/people/people.tsx
@@ -11,7 +11,7 @@ export default function People({
 }) {
   const router = useRouter();
 
-  const handleCardClick = (speakerId: string) => {
+  const handleCardClick = (speakerId: number) => {
     router.push(`/person?conf=${conf.code}&person=${speakerId}`);
   };
 
