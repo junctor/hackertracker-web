99435f0dd0877f2ff6bba82861122397df6d057a
cak <cak@typeerror.com>
2025-09-06 06:12:33 -0400

    refactor: update styles import path and add styles.css for Tailwind integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles.css";
+import "./styles/styles.css";
 import AppRouter from "./app/router.tsx";
 import { BrowserRouter } from "react-router";
 
